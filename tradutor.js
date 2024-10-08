adbad6e8330bd9a589df6f092dec0f97964ee6bd
jotave <164882755+jvbarbosa06@users.noreply.github.com>
2024-10-08 11:49:38 -0300

    Add files via upload

---
+++ tradutor.js
@@ -0,0 +1,46 @@
+const textareaFrom = document.querySelector("#textareaFrom");
+const textareaTo = document.querySelector("#textareaTo");
+const btnTranslate = document.querySelector("#btnTranslate");
+const selects = document.querySelectorAll("select");
+
+const countries = {
+    "en-GB": "Inglês",
+    "es-ES": "Espanhol",
+    "it-IT": "Italiano",
+    "ja-JP": "Japonês",
+    "pt-BR": "Português",
+
+};
+
+
+selects.forEach((tag) => {
+    for (let country in countries) {
+        let selected;
+        if (tag.className.includes("selectFrom") && country == "pt-BR") {
+            selected = "selected";
+        }
+
+        const option = '<option value= "${country}" ${selected}>${countries[country]}</option>';
+        tag.insertAdjacentHTML("beforeend", option);
+
+
+    }
+});
+
+btnTranslate.addEventListener("Click", () => {
+    if (textareaFrom.value) {
+        loadTranslation();
+    } else {
+        textareaTo.value = "";
+    }
+});
+
+function loadTranslation() {
+    fetch(
+        'https://api.memory.translated.net/get?q=${textareaFrom.value}&langpair=${selects[0].value}|${selects[1].value}'
+    ).then((res) => res.json())
+    .then((data) => {
+        textareaTo.value = data.responseData.translatedText;
+    })
+}
+
